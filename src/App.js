9e8181718ac51b937e6d2f1343262cf37a617edf
Gabriela da Rosa Neto <gabriela.netor94@gmail.com>
2020-08-11 11:24:12 -0300

    evolution2.0

--- src/App.js
+++ src/App.js
@@ -5,36 +5,30 @@ import { TextField, Button } from "@material-ui/core"
 function App() {
   return (
     <div>
-      <Formik initialValues={{ Nome: '', Sobrenome: '' }} 
+    <Formik 
+      initialValues={{ Nome: '', Sobrenome: '' }} 
       onSubmit={( data, { setSubmitting }) => {
         setSubmitting(true);
         // chamada assync
         console.log('submit: ', data);
         setSubmitting(false);
       }}
-      >
-        {({ values, isSubmitting, handleChange, handleBlur, handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            <Field name='Nome' type='input' as={TextField} />
-            <TextField 
-              name="Nome" 
-              value={values.Nome} 
-              onChange={handleChange} 
-              onBlur={handleBlur} 
-            />
-            <TextField 
-              name="Sobrenome" 
-              value={values.Sobrenome} 
-              onChange={handleChange} 
-              onBlur={handleBlur} 
-            />
-            <Button type="submit">Enviar</Button>
-            <pre>
-              {JSON.stringify(values, null, 2)}
-            </pre>
-          </form>
-        )}
-      </Formik>
+    >
+      {({ values, isSubmitting, handleChange, handleBlur, handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <Field placeholder='Nome' name='Nome' type='input' as={TextField} />
+          <Field placeholder='Sobrenome' name='Sobrenome' type='input' as={TextField} />
+          <div>
+            <Button disabled={isSubmitting} type="submit">
+              Enviar
+            </Button>
+          </div>
+          <pre>
+            {JSON.stringify(values, null, 2)}
+          </pre>
+        </form>
+      )}
+    </Formik>
     </div>
   );
 };
