40320f752c5f30504b7d60124ce6f40b5d0ae73b
Gabriela da Rosa Neto <gabriela.netor94@gmail.com>
2020-08-11 14:52:27 -0300

    hook useField()

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Formik, Field } from 'formik';
-import { TextField, Button } from "@material-ui/core"
+import { Formik, Field, Form, useField } from 'formik';
+import { TextField, Button, Checkbox, Radio, FormControlLabel } from "@material-ui/core"
+
+const MeuRadio = ({label, ...props}) => {
+  const [field, meta] = useField(props);
+  //field.
+  return (
+    // <FormControlLabel value={field.value} onChange={field.onChange} control={<Radio />} label={label} />
+    <FormControlLabel {...field} control={<Radio />} label={label} />
+  )
+}
 
 function App() {
   return (
     <div>
     <Formik 
-      initialValues={{ Nome: '', Sobrenome: '' }} 
+      initialValues={{ 
+        Nome: '', 
+        Sobrenome: '', 
+        isActivated: false, 
+        cookies: [], 
+        yogurt: '' 
+      }} 
       onSubmit={( data, { setSubmitting }) => {
         setSubmitting(true);
         // chamada assync
@@ -14,10 +29,22 @@ function App() {
         setSubmitting(false);
       }}
     >
-      {({ values, isSubmitting, handleChange, handleBlur, handleSubmit }) => (
-        <form onSubmit={handleSubmit}>
+
+      {({ values, isSubmitting }) => (
+        <Form>
           <Field placeholder='Nome' name='Nome' type='input' as={TextField} />
-          <Field placeholder='Sobrenome' name='Sobrenome' type='input' as={TextField} />
+          <div>
+            <Field placeholder='Sobrenome' name='Sobrenome' type='input' as={TextField} />
+          </div>
+          <Field name="isActivated" type="checkbox" as={Checkbox} />
+          <div>cookies:</div>
+          <Field name="cookies" type="checkbox" value="gotas de chocolate" as={Checkbox} />
+          <Field name="cookies" type="checkbox" value="frutas vermelhas" as={Checkbox} />
+          <Field name="cookies" type="checkbox" value="nutella" as={Checkbox} />
+          <div>yogurt: </div>
+          <Field name="yogurt" type="radio" value="soja" as={Radio} />
+          <Field name="yogurt" type="radio" value="maçã" as={Radio} />
+          <Field name="yogurt" type="radio" value="blueberry" as={Radio} />
           <div>
             <Button disabled={isSubmitting} type="submit">
               Enviar
@@ -26,7 +53,7 @@ function App() {
           <pre>
             {JSON.stringify(values, null, 2)}
           </pre>
-        </form>
+        </Form>
       )}
     </Formik>
     </div>
